b887ba3638769699b6245c59f8597c2c4523b50a
AinhoaSalo <ainhoahsalo@gmail.com>
2022-04-12 08:12:29 +0200

    Delete folders not necessary for responsive

--- src/fetch/ListTravel/createYourList/CreateYourList.js
+++ src/fetch/ListTravel/createYourList/CreateYourList.js
@@ -1,16 +1,10 @@
 import "./CreateYourList.css"
-import CreateSmartPhone from "./createSmartPhone/CreateSmartPhone";
-import CreateMobile from "./createMobile/CreateMobile";
-import CreateDesktop from "./createDesktop/CreateDesktop";
 import CreateDesktopMaxPixel from "./createDesktopMaxPixel/CreateDesktopMaxPixel";
 
 function CreateYourList() {
 
   return (
     <>
-      <CreateSmartPhone/>
-      <CreateMobile/>
-      <CreateDesktop/>
       <CreateDesktopMaxPixel/>
     </>
   );
