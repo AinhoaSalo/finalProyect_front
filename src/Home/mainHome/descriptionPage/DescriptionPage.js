f12270bd497fe0b7004f00b5eb9b9d7a5ff73bc1
AinhoaSalo <ainhoahsalo@gmail.com>
2022-04-18 14:31:44 +0200

    Modify responsive

--- src/Home/mainHome/descriptionPage/DescriptionPage.js
+++ src/Home/mainHome/descriptionPage/DescriptionPage.js
@@ -7,24 +7,11 @@ function DescriptionPage() {
       <>
         <Parallax bgImage={firstImgParallax} strength={600}>
         <div className='styleParallaxFirst'></div>
-        <div className='descriptionPageSmartPhone'>
-          <h2 className='titleDescripcionPage'>¡Planea tu viaje!</h2>
-          <p className='parrafDescripcionPage'>¿Te gusta tener todo organizado para que no se te olvide nada en tus viajes? A mi también, de ahí nace PlanYourTrip. Cuando viajo me gusta tener todo detallado ya que (me ha ocurrido) puede que se te olvide ese sitio tan guay que llevas tiempo queriendo ir o bar donde sirven tu cerveza favorita.</p>
-        </div>
-        <div className='descriptionPageMobile'>
-          <h2 className='titleDescripcionPage'>¡Planea tu viaje!</h2>
-          <p className='parrafDescripcionPage'>¿Te gusta tener todo organizado para que no se te olvide nada en tus viajes? A mi también, de ahí nace PlanYourTrip. Cuando viajo me gusta tener todo detallado ya que (me ha ocurrido) puede que se te olvide ese sitio tan guay que llevas tiempo queriendo ir o bar donde sirven tu cerveza favorita.</p>
-        </div>
-        <div className='descriptionPageDesktop'>
-          <h2 className='titleDescripcionPage'>¡Planea tu viaje!</h2>
-          <p className='parrafDescripcionPage'>¿Te gusta tener todo organizado para que no se te olvide nada en tus viajes? A mi también, de ahí nace PlanYourTrip. Cuando viajo me gusta tener todo detallado ya que (me ha ocurrido) puede que se te olvide ese sitio tan guay que llevas tiempo queriendo ir o bar donde sirven tu cerveza favorita.</p>
-        </div>
-        <div className='descriptionPageDesktopMaxPixel'>
+        <div className='descriptionPage'>
           <h2 className='titleDescripcionPage'>¡Planea tu viaje!</h2>
           <p className='parrafDescripcionPage'>¿Te gusta tener todo organizado para que no se te olvide nada en tus viajes? A mi también, de ahí nace PlanYourTrip. Cuando viajo me gusta tener todo detallado ya que (me ha ocurrido) puede que se te olvide ese sitio tan guay que llevas tiempo queriendo ir o bar donde sirven tu cerveza favorita.</p>
         </div>
         </Parallax>
-  
       </>
     )
   
