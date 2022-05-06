c9be99308a66210a706786cf99ec32ff29b52828
AinhoaSalo <ainhoahsalo@gmail.com>
2022-05-06 11:55:43 +0200

    Pin up header, change logos footer div -> ul and li

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import ListTravelFetch from './fetch/ListTravel/ListTravelFetch/ListTravelFetch'
 import CreateYourList from './fetch/ListTravel/createYourList/CreateYourList';
 import UsersRegisterLogin from './fetch/users/registerLogin/UsersRegisterLogin';
 import UserArea from './fetch/users/deleteput/UserArea';
-import Footer from './Home/footer/Footer';
+import {Footer}from './Home/footer/Footer';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Router>
-        <Header/>
+        <header>
+          <Header/>
+        </header>
         <main>
           <Routes>
             <Route path="/" element={<Navigate to="/inicio" replace />}/>
@@ -27,7 +29,9 @@ function App() {
             <Route path='/registro'  element={<UsersRegisterLogin/>}/>
           </Routes>
         </main>
-        <Footer/>
+        <footer>
+          <Footer/>
+        </footer>
       </Router>
     </>
   );
--- src/Home/footer/Footer.js
+++ src/Home/footer/Footer.js
@@ -3,20 +3,20 @@ import logo from './img/srtasalogris.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-function Footer() {
+export function Footer() {
   return(
     <>
       <div className='footer'>
         <div className='logoSrtaSalo'><img className='imgLogoSrtaSalo' src={logo} alt="Logo SrtaSalo" /></div>
         <div className='socialMediaTitle'><h3>Redes sociales</h3></div>
-        <div className='socialMedia'>
-          <a className='linkGithub' href="https://github.com/AinhoaSalo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={brands('github')} size="2x" /></a>
-          <a className='linkLinkedin' href="https://www.linkedin.com/in/ainhoa-hernandez-salo/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={brands('linkedin')} size="2x" /></a>
-          <a className='linkTwitter' href="https://twitter.com/SrtaSalo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={brands('twitter')} size="2x" /></a>
-        </div>
+        <ul className='socialMedia'>
+          <li><a className='linkGithub' href="https://github.com/AinhoaSalo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={brands('github')} size="2x" /></a></li>
+          <li><a className='linkLinkedin' href="https://www.linkedin.com/in/ainhoa-hernandez-salo/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={brands('linkedin')} size="2x" /></a></li>
+          <li><a className='linkTwitter' href="https://twitter.com/SrtaSalo" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={brands('twitter')} size="2x" /></a></li>
+        </ul>
       </div>
     </>
   )
 }
   
-export default Footer;
+// export default Footer;
